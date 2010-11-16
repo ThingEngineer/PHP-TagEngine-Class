801176ebf5a664b7e7a8a5f240e30893d6cce8ee
Josh Campbell <github@teslacity.com>
2010-11-15 23:38:05 -0600

    converted to a codeigniter model

--- TagEngine.class.php
+++ TagEngine.class.php
@@ -1,329 +1,386 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * TagEngine Class
+ * Tags Class
  *
  * @category Data Mapping
- * @package TagEngine
+ * @package Tags
  * @author Josh Campbell <josh.campbell@teslacity.com>
  * @copyright Copyright (c) 2010, Josh Campbell
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
  * @version 1.0
  **/
-class TagEngine
+class Tags extends Model
 {	
-	/**
-	 * MySqliDb instance
-	 *
-	 * @var mixed
-	 */
-	protected $db;
 	/**
 	 * Name of the tags table in the database
 	 *
 	 * @var string
 	 */
-	protected $tableTags = 'sysTags';
+	protected $table_tags = 'tags';
 	/**
 	 * Name of the tagmap types table in the database
 	 *
 	 * @var string
 	 */
-	protected $tableTagTypes = 'sysTagTypes';
+	protected $table_tag_types = 'tag_types';
 	/**
 	 * Name of the tagmaps table in the database
 	 *
 	 * @var string
 	 */
-	protected $tableTagMaps = 'sysTagMaps';
+	protected $table_tag_maps = 'tag_maps';
 	/**
 	 * The minimum number of characters a tag can have.
 	 *
 	 * @var integer
 	 */
-	protected $_tagLen = 2;
+	protected $_tag_len = 2;
 	
 	/**
-	 * Initialize TagEngine - Load database instance
+	 * Initialize Tag Model - Get CI instance
 	 */
-	public function __construct()
+	public function Tags()
 	{
-		$this->db = MySqliDb::getInstance();
+		$this->CI =& get_instance();
 	}
 	
 	/**
-	 * Set/Change the TagEngine tables.
+	 * Set/Change the tag tables.
 	 * 
-	 * @uses tagEngine->setTables('myAppTags', 'myAppTagTypes', 'myAppTagMaps')
+	 * @uses this->set_tables('pre_tags', 'pre_tag_types', 'pre_tag_maps')
 	 *
 	 * @param string $tableTags The name of the tags table.
-	 * @param string $tableTagTypes The name of the tag types table.
-	 * @param string $tableTagMaps The name of the tag maps table.
+	 * @param string $table_tag_types The name of the tag types table.
+	 * @param string $table_tag_maps The name of the tag maps table.
 	 */
-	public function setTables($tableTags, $tableTagTypes, $tableTagMaps)
+	public function set_tables($table_tags, $table_tag_types, $table_tag_maps)
 	{
-		$this->tableTags		= $tableTags;
-		$this->tableTagTypes	= $tableTagTypes;
-		$this->tableTagMaps		= $tableTagMaps;
+		$this->table_tags		= $table_tags;
+		$this->table_tag_types	= $table_tag_types;
+		$this->table_tag_maps		= $table_tag_maps;
 	}
 	
 	/**
 	 * Set/Change the minimum tag length.
 	 * 
-	 * @uses tagEngine->setTagLen(2)
+	 * @uses this->set_tag_len(2)
 	 *
-	 * @param integer $tagLen The minimum number of characters a tag can have.
+	 * @param integer $tag_len The minimum number of characters a tag can have.
 	 */
-	public function setTagLen($tagLen)
+	public function set_tag_len($tag_len)
 	{
-		$this->_tagLen = $tagLen;
+		$this->_tag_len = $tag_len;
 	}
 
 	/**
 	 * Get the id of a tag name.
 	 * 
-	 * @uses TagEngine->getTagId('tagName')
+	 * @uses this->get_tag_id('tagName')
 	 *
 	 * @param string $name The tag name.
-	 * @return mixed Returns the tagId if the tag exists, false if it does not.
+	 * @return mixed Returns the tag_id if the tag exists, false if it does not.
 	 */
-	public function getTagId($name)
+	public function get_tag_id($name)
 	{
-		$this->db->where('name',trim($name));
-		$result = $this->db->query("SELECT tagId FROM $this->tableTags",1);
+		$this->CI->db->select('tag_id')
+			->where('name', trim($name));
+		$query = $this->CI->db->get($this->table_tags, 1);
 		
-		if (!empty($result)) {
-			$tagId = $result[0]['tagId'];
-			return $tagId;
-		} else {
-			return false;
+		if ($query->num_rows() > 0)
+		{
+		   $row = $query->row(); 
+		   return $row->tag_id;
+		}
+		else
+		{
+			return FALSE;
 		}
 	}
 
 	/**
 	 * Does the tag name exist?
 	 * 
-	 * @uses TagEngine->tagExists('tagName')
+	 * @uses this->tag_exists('tagName')
 	 *
 	 * @param string $name The tag name.
-	 * @return boolean Returns true if the tag exists, false if it does not.
+	 * @return boolean Returns TRUE if the tag exists, FALSE if it does not.
 	 */	
-	public function tagExists($name)
+	public function tag_exists($name)
 	{
-		($this->getTagId($name)) ? $result = true : $result = false;
+		($this->get_tag_id($name)) ? $result = TRUE : $result = FALSE;
 		return $result;
 	}
 
 	/**
 	 * Get the tag type's id.
 	 * 
-	 * @uses TagEngine->getTagTypeId('type')
+	 * @uses this->get_tag_type_id('type')
 	 *
-	 * @param string $type The 3 character tag type name. (Not to be confused with the longName.)
-	 * @return mixed Returns the typeId if the tag type exists, false if it does not.
+	 * @param string $type The 3 character tag type name. (Not to be confused with the long_name.)
+	 * @return mixed Returns the type_id if the tag type exists, false if it does not.
 	 */
-	public function getTagTypeId($type)
+	public function get_tag_type_id($type)
 	{
-		$this->db->where('name',trim($type));
-		$result = $this->db->query("SELECT typeId FROM $this->tableTagTypes",1);
+		$this->CI->db->select('type_id')
+			->where('name', trim($type));
+		$query = $this->CI->db->get($this->table_tag_types, 1);
 		
-		if (!empty($result)) {
-			$tagId = $result[0]['typeId'];
-			return $tagId;
-		} else {
-			trigger_error('Invalid tag type: "'.$type.'"', E_USER_ERROR);
+		if ($query->num_rows() > 0)
+		{
+		   $row = $query->row(); 
+		   return $row->type_id;
+		}
+		else
+		{
+			log_message('error', 'Invalid tag type: "'.$type.'"');
+			return FALSE;
 		}
 	}
 
 	/**
-	 * Get the tagId/tags for an entity of the given relative id and type.
+	 * Get the tag_id/tags for an entity of the given relative id and type.
 	 * 
-	 * @uses TagEngine->getTags(relId, type)
+	 * @uses this->get_tags(rel_id, type)
 	 *
-	 * @param integer $relId The id of the entity being searched.
-	 * @param string $type The 3 character tag type name being searched. (Not to be confused with the longName.)
-	 * @return array Returns an array of string containing the tags where the key is the tagId and the data is the tag name.
+	 * @param integer $rel_id The id of the entity being searched.
+	 * @param string $type The 3 character tag type name being searched. (Not to be confused with the long_name.)
+	 * @return array Returns an array of string containing the tags where the key is the tag_id and the data is the tag name.
 	 */
-	public function getTags($relId, $type)
+	public function get_tags($rel_id, $type)
 	{
-		$tagsArr = array();	// Temporary tags array.
+		$tags_arr = array();	// Temporary tags array.
 		
-		$typeId = $this->getTagTypeId($type);
+		$type_id = $this->get_tag_type_id($type);
 		
-		$this->db->where('typeId', $typeId);
-		$this->db->where('relId', $relId);
-		$result = $this->db->query("SELECT tagId FROM $this->tableTagMaps");
-		foreach($result as $row) {
-			$tagsArr[$row['tagId']] = htmlentities(stripslashes($this->getTagName($row['tagId'])));
-			//array_push($tagsArr,$this->getTagName($row['tagId']));
+		$this->CI->db->select('tag_id')
+			->where('type_id', $type_id)
+			->where('rel_id', $rel_id);
+		$query = $this->CI->db->get($this->table_tag_maps);
+		
+		foreach($query->result() as $row)
+		{
+			// TODO: convert to join
+			$tags_arr[$row->tag_id] = htmlentities($this->get_tag_name($row->tag_id));
+			//array_push($tags_arr,$this->get_tag_name($row->tag_id));
 		}
-		return $tagsArr;
+		
+		return $tags_arr;
 	}
 	
 	/**
-	 * Get all tagId/tags.
+	 * Get all tag_id/tags.
 	 * 
-	 * @uses TagEngine->getAllTags()
+	 * @uses this->get_all_tags()
 	 *
-	 * @return array Returns an array of strings containing the tags where the key is the tagId and the data is the tag name.
+	 * @return array Returns an array of strings containing the tags where the key is the tag_id and the data is the tag name.
 	 */
-	public function getAllTags()
+	public function get_all_tags()
 	{
-		$tagsArr = array();	// Temporary tags array.
+		$tags_arr = array();	// Temporary tags array.
 		
-		$result = $this->db->get($this->tableTagMaps);
-		foreach($result as $row) {
-			$tagsArr[$row['tagId']] = $this->getTagName($row['tagId']);
-			//array_push($tagsArr,$this->getTagName($row['tagId']));
+		$query = $this->CI->db->get($this->table_tag_maps);
+		foreach($query->result() as $row) {
+			$tags_arr[$row->tag_id] = $this->get_tag_name($row->tag_id);
+			//array_push($tags_arr,$this->get_tag_name($row->tag_id));
 		}
-		return $tagsArr;
+		return $tags_arr;
 	}
-
+	
+	
+	/**
+	 * Get all tag_id/tags.
+	 * 
+	 * @uses this->get_all_tags_cvs()
+	 *
+	 * @return array Returns a comma seperated string containing all tags.
+	 */
+	public function get_all_tags_cvs()
+	{
+		$tags = $this->get_all_tags();
+		return implode('","', $tags);
+	}
+	
+	
 	/**
 	 * Get the tag name.
 	 * 
-	 * @uses TagEngine->getTagName(tagId)
+	 * @uses this->get_tag_name(tag_id)
 	 *
-	 * @param integer $tagId The id of the tag to be retrived.
-	 * @return string Returns the tag name of the given tagId.
+	 * @param integer $tag_id The id of the tag to be retrived.
+	 * @return string Returns the tag name of the given tag_id.
 	 */	
-	public function getTagName($tagId)
+	public function get_tag_name($tag_id)
 	{
-		$this->db->where('tagId', $tagId);
-		$result = $this->db->query("SELECT name FROM $this->tableTags",1);
-		return $result[0]['name'];
+		$this->CI->db->select('name', $tag_id)
+			->where('tag_id', $tag_id);
+		$query = $this->CI->db->get($this->table_tags, 1);
+		
+		if ($query->num_rows() > 0)
+		{
+		   $row = $query->row(); 
+		   return $row->name;
+		}
+		else
+		{
+			return FALSE;
+		}
 	}
-
+	
+	
 	/**
 	 * Adds tags to the database if they do not already exist and creates tag maps to them. 
 	 * 
-	 * @uses TagEngine->mapTags(105, 'img', 'tag1,tag2,tag3')
+	 * @uses this->map_tags(105, 'img', 'tag1,tag2,tag3')
 	 *
-	 * @param integer $relId The id of the entity being tagged.
+	 * @param integer $rel_id The id of the entity being tagged.
 	 * @param string $type The 3 character type.
-	 * @param string $tags A comma delimited string of tags to be mapped to the relId.
-	 * @return array Returns an array containing 2 other arrays, [tagIDs] and [mapIds] each contains the id's to their respective referances for each tag/map created. 
+	 * @param string $tags A comma delimited string of tags to be mapped to the rel_id.
+	 * @return array Returns an array containing 2 other arrays, [tag_ids] and [map_ids] each contains the id's to their respective referances for each tag/map created. 
 	 */
-	public function mapTags($relId, $type, $tags)
+	public function map_tags($rel_id, $type, $tags)
 	{
-		$tagIds = array();
-		$mapIds = array();
+		$new_tags_arr = array();
+		$tag_ids = array();
+		$map_ids = array();
 		
-		$tagsArr = explode(',', $tags);
+		$tags_arr = explode(',', $tags);
 		
-		$typeId = $this->getTagTypeId($type);
+		$type_id = $this->get_tag_type_id($type);
 		
 		$i = 0;
-		foreach ($tagsArr as $tag) {
+		foreach ($tags_arr as $tag) {
 			$tag = trim($tag);
-			if (strlen($tag) >= $this->_tagLen)	{						// Tag must be at least X characters.
-				$tagId = $this->getTagId($tag);							// If the tag exists get its ID.
-				if (!$tagId) $tagId = $this->insertTag($tag);			// If not, create the tag and get the new id.
-				array_push($tagIds, $tagId);
+			if (strlen($tag) >= $this->_tag_len)							// Tag must be at least X characters.
+			{
+				array_push($new_tags_arr, $tag);
 				
-				$result = $this->addTagMap($relId, $typeId, $tagId);	// Add tag map for this tag if it does not already exist.
-				array_push($mapIds, $result);
+				$tag_id = $this->get_tag_id($tag);							// If the tag exists get its ID.
+				if (!$tag_id) $tag_id = $this->insert_tag($tag);			// If not, create the tag and get the new id.
+				array_push($tag_ids, $tag_id);
+				$result = $this->add_tag_map($rel_id, $type_id, $tag_id);	// Add tag map for this tag if it does not already exist.
+				array_push($map_ids, $result);
 			}
 			$i++;
 		}
-		$results = array('tagIds' => $tagIds, 'mapIds' => $mapIds);		// Build the id arrays
+		$results = array('tags' => $new_tags_arr, 'tag_ids' => $tag_ids, 'map_ids' => $map_ids);		// Build the id arrays
 		return $results;
 	}
 
 	/**
 	 * Insert a tag
 	 * 
-	 * @uses TagEngine->insertTag('tagname')
+	 * @uses this->insert_tag('tagname')
 	 *
 	 * @param string $name The tag to be inserted.
 	 * @return integer Returns the id of the tag that was inserted.
 	 */
-	protected function insertTag($name)
+	protected function insert_tag($name)
 	{
-		$insertData = array('name' => $name);
-		return $this->db->insert($this->tableTags, $insertData);
+		$insert_data = array('name' => $name);
+		$this->CI->db->insert($this->table_tags, $insert_data);
+		return $this->CI->db->insert_id();
 	}
 
 	/**
 	 * Creates a map entry between a relative id, tag, and type if one does not already exist.
 	 * 
-	 * @uses TagEngine->addTagMap(relId,typeId,tagId)
+	 * @uses this->add_tag_map(rel_id,type_id,tag_id)
 	 *
-	 * @param integer $relId The id of the entity being tagged.
-	 * @param integer $typeId The id of the type of tag being mapped.
-	 * @param integer $tagId The id of the tag being mapped to.
+	 * @param integer $rel_id The id of the entity being tagged.
+	 * @param integer $type_id The id of the type of tag being mapped.
+	 * @param integer $tag_id The id of the tag being mapped to.
 	 * @return integer Returns the id of the map that was inserted or false if it exists or there was an error inserting.
 	 */
-	protected function addTagMap($relId, $typeId, $tagId)
+	protected function add_tag_map($rel_id, $type_id, $tag_id)
 	{
-		$this->db->where('relId', $relId);
-		$this->db->where('typeId', $typeId);
-		$this->db->where('tagId', $tagId);
-		$result = $this->db->query("SELECT id FROM $this->tableTagMaps",1);		// Check for existing map.
-		if (empty($result)) {
-			$insertData = array(
-				'relId'  => $relId,
-				'typeId' => $typeId,
-				'tagId'  => $tagId);
-			$result = $this->db->insert($this->tableTagMaps, $insertData);		// Add new map.
-		} else {
-			$result = $result[0]['id'];		// Get the id of existing the map.
+		$this->CI->db->select('id')
+			->where('rel_id', $rel_id)
+			->where('type_id', $type_id)
+			->where('tag_id', $tag_id);
+		$query = $this->CI->db->get($this->table_tag_maps, 1);			// Check for existing map.
+		
+		if ($query->num_rows() == 0)
+		{
+		   	$insert_data = array(
+				'rel_id'  => $rel_id,
+				'type_id' => $type_id,
+				'tag_id'  => $tag_id);
+			$result = $this->CI->db->insert($this->table_tag_maps, $insert_data);		// Add new map.
 		}
+		else
+		{
+			$row = $query->row();
+			$result = $row->id;			// Get the id of existing the map.
+		}
+		
 		return $result;
 	}
 
 	/**
 	 * When deleting an entity, use this method to remove its tag maps and delete the tags mapped to it if they are no longer in use.
 	 * 
-	 * @uses TagEngine->cleanTags(relId,type)
+	 * @uses this->clean_tags(rel_id,type)
 	 *
-	 * @param integer $relId The id of the entity being cleaned.
-	 * @param integer $typeId The type of tag being cleaned.
+	 * @param integer $rel_id The id of the entity being cleaned.
+	 * @param integer $type_id The type of tag being cleaned.
 	 */
-	public function cleanTags($relId, $type)
+	public function clean_tags($rel_id, $type)
 	{
-		$tagsArr = $this->getTags($relId, $type);
-		foreach($tagsArr as $tag) {							// Loop through each tag mapped to this entity and remove the mapping.
-			$tagId = $this->getTagId($tag);
-			$this->removeTagMap($relId, $type, $tagId);
+		$tags_arr = $this->get_tags($rel_id, $type);
+		foreach($tags_arr as $tag) {							// Loop through each tag mapped to this entity and remove the mapping.
+			$tag_id = $this->get_tag_id($tag);
+			$this->remove_tag_map($rel_id, $type, $tag_id);
 		}
 	}
 
 	/**
 	 * Remove a tag map and delete the tag it was mapped to if that tag is no longer mapped to any other entity of any type.
 	 * 
-	 * @uses TagEngine->removeTagMap(relId,typeId,tagId)
+	 * @uses this->remove_tag_map(rel_id,type_id,tag_id)
 	 *
-	 * @param integer $relId The id of the entity map being removed.
+	 * @param integer $rel_id The id of the entity map being removed.
 	 * @param integer $type The 3 character string representing the the type of tag being removed.
-	 * @param integer $tagId The id of the tag being unmapped.
+	 * @param integer $tag_id The id of the tag being unmapped.
 	 */
-	public function removeTagMap($relId, $type, $tagId)
+	public function remove_tag_map($rel_id, $type, $tag_id)
 	{
-		$typeId = $this->getTagTypeId($type);
+		$type_id = $this->get_tag_type_id($type);
 		
-		$this->db->where('relId', $relId);
-		$this->db->where('typeId', $typeId);
-		$this->db->where('tagId', $tagId);
-		$this->db->delete($this->tableTagMaps, 1);			// Remove the tag map.
+		$this->CI->db->where('rel_id', $rel_id)
+			->where('type_id', $type_id)
+			->where('tag_id', $tag_id)
+			->delete($this->table_tag_maps);			// Remove the tag map.
 		
-		$this->db->where('tagId', $tagId);
-		$results = $this->db->query("SELECT id FROM $this->tableTagMaps");
-		if (empty($results))	{						// If the tag is no longer in use, delete it as well.
-			$this->removeTag($tagId);
+		$this->CI->db->select('id')
+			->where('tag_id', $tag_id);
+		$query = $this->CI->db->get($this->table_tag_maps, 1);	// Is this tag in use in any other map?
+		
+		if ($query->num_rows() == 0)					// If the tag is no longer in use, delete it as well.
+		{
+			$this->remove_tag($tag_id);
+		}
+		
+		if ($this->CI->db->affected_rows() > 0)
+		{
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
 		}
 	}
 
 	/**
 	 * Removes a tag.
 	 * 
-	 * @uses TagEngine->removeTag(tagId)
+	 * @uses this->remove_tag(tag_id)
 	 *
-	 * @param integer $tagId The id of the tag being removed.
+	 * @param integer $tag_id The id of the tag being removed.
 	 */
-	protected function removeTag($tagId)
+	protected function remove_tag($tag_id)
 	{
-		$this->db->where('tagId', $tagId);
-		$this->db->delete($this->tableTags, 1);
+		$this->CI->db->where('tag_id', $tag_id)
+			->delete($this->table_tags);
 	}
 
 } // END class
